27dd63a3b33800db619ce5bd288bb56539ec012c
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-22 01:31:09 +0200

    s

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -2,3 +2,14 @@
 import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
+a = "is positive"
+b = "is zero"
+c = "is negative"
+
+if number > 0:
+  print("is positive")
+if number == 0:
+  print("is zero")
+else: 
+  print("is negative")
+  
