cb18d63e9dcce86faee5cf4ef87da60ba2e89fb1
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-23 21:48:56 +0100

    n

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,13 +6,12 @@ Last_digit = (number) % 10
 
 if number < 0:
     Last_digit == -Last_digit
- 
 
 print ("Last_digit of {}" "is {}". format(number,Last_digit),end="")
 
 if Last_digit > 5:
     print("and is greater than 5")
-elif Last_digit == 0 and Last_digit < 0:
+elif Last_digit == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
