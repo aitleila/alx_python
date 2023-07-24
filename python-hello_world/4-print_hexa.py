5a4204e5d30ac231263fd152595cc11a06d7cb38
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-25 00:59:58 +0100

    l

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,9 +1,6 @@
-  # take input from user
 n = 98
- 
-# loop through the numbers from 1 to N
 for i in range(0, n+1):
-    # use string formatting to convert the number to hexadecimal
+
     hex_value = "{0:x}".format(i)
-    # print the hexadecimal value
+
     print(hex_value)
