e43b7b884cefbb1aeb60b3d56db7887fce08e4af
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-25 16:49:28 +0100

    k

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,5 +2,5 @@
 word = "Holberton"
 
 print("First 3 letters: {}".format(word[:3]))
-print("Last 2 letters: {}".format(word[7:]))
+print("Last 2 letters: {}".format(word[-2:]))
 print("Middle word: {}".format(word[1:8]))
