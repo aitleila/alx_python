8712c6b197a23ddc1dc2e7703224672373d2102d
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-25 16:40:08 +0100

    j

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-# YOUR CODE HERE
-a = 98
 
-if a > 0:
+if number > 0:
   print("98 is positive")
-elif a == 0:
+elif number == 0:
   print("0 is zero")
 else: 
   print("-98 is negative")
+
 
