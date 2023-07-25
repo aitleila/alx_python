51ffad14ad3b44b8464ed12aa6b403bc7a2576d5
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-25 02:39:45 +0100

    j

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import random
 number = random.randint(-10000, 10000)
 Last_digit = abs(number) % 10
@@ -8,6 +10,6 @@ elif Last_digit == 0:
     print("and is 0")
 else:
     print("and is less than 6 and not 0")
-    
+
 
 
