3e846d0402495b42d47c285f94747ace07001e69
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-25 02:31:37 +0100

    k

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,12 +3,10 @@ import random
 number = random.randint(-10000, 10000)
 
 Last_digit = (number % 10)
-
+Last_digit = abs(number) % 10
 
 if number < 0:
     Last_digit == -Last_digit
-    Last_digit = abs(number) % 10
-
 
 print ("Last_digit of {}" "is {}". format(number,Last_digit),end="")
 
