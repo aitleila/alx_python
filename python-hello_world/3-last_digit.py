3845880b5562cb6e66e1cc89c38b9350c182ca86
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-25 15:51:51 +0100

    h

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -23,6 +23,8 @@ if number < 0:
 
     if Last_digit > 5:
          print(" and is greater than 5 ")
+    elif Last_digit == 0:
+        print(" and is 0 ")
     else:
         print(" and is less than 6 and not 0 ")
 
