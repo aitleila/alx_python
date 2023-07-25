b53185e8cfcfc3af852bf9493e28d1763277cc1e
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-25 15:19:38 +0100

    n

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -16,9 +16,10 @@ if number > 0:
     else:
         print(" and is less than 6 and not 0 ")
 
-elif number < 0:
-    Last_digit = -Last_digit
+elif number <= 0:
+Last_digit = -Last_digit
 
+   
     if Last_digit == 0:
         print(" and is 0 ")
     else:
