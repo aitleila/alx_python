550fae4a32083685c47dc4ab489a594ee52f6d38
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-25 16:51:14 +0100

    h

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-word = "Holberton"
+word = "school"
 
 print("First 3 letters: {}".format(word[:3]))
 print("Last 2 letters: {}".format(word[-2:]))
-print("Middle word: {}".format(word[1:8]))
+print("Middle word: {}".format(word[1:-1]))
