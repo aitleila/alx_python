fd0277402b8889c79e955559de39bd8d4b311894
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-26 02:45:24 +0100

    j

---
+++ python-functions/4-fibonacci.py
@@ -0,0 +1,14 @@
+def fibonacci_sequence(n):
+    if n == 0:
+      return []
+    elif n == 1:
+      return [0]
+    elif n == 2:
+       return [0, 1]
+    
+    fib_seq = [0, 1]
+    for i in range(2, n):
+        next_fib = fib_seq[-1] + fib_seq[-2]
+        fib_seq.append(next_fib)
+
+    return fib_seq
