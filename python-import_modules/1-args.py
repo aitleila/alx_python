487f16c417898b312b56fd79df1da6c9d522fd4a
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-28 02:42:01 +0100

    k

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -2,15 +2,15 @@
 
 import sys
 
+
 if __name__ == "__main__":
     args = sys.argv[1:]
-    num_args=len(args)
+    num_args = len(args)
 
     if num_args == 0:
-       print ("0 arguments.")
-
+        print("0 arguments.")
     else:
-        print (f"{num_args}arguments{''if num_args > 1 else ''}:")
+        print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
         for i, arg in enumerate(args, 1):
-            print(f"{i}:{arg}")
+            print(f"{i}: {arg}")
 
