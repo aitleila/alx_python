1ff2a99d2e3a247f8a479a2c6c76a8fded2e6d3f
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-28 15:51:05 +0100

    s

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 def no_c(my_string):
    
-    my_new_string = my_string.replace("c", "", "")
+   result = ""
 
-    return my_new_string
+for char in my_string:
+    if char not in ('c', 'C'):
+       result += char
+
+return result
