e47e45eda47b71250121132778e4fd848ba8a8bb
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-28 02:54:32 +0100

    h

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
-import variable_load_2
-
-def main():
-    global a
-    a = 98
-
-    result = a
-    print("{} = {}".format(a, result))
-
 if __name__ == "__main__":
-    main()
+    from variable_load_2 import a
+    print(a)
+
