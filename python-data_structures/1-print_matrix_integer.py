62c5eef89a9727f9d975455750e5a781f2974455
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-29 16:31:30 +0100

    m

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -4,8 +4,8 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, number in enumerate(row):
             if i ==len(row) - 1:
-                print ("{:d}".format [[number]])
+                print ("{:d}".format {} {} {})
             else:
-                print ("{:d}".format [[number]])
+                print ("{:d}".format {} {} {})
     print()
     
