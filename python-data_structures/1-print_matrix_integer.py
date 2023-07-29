7825f85d71f56bf8bd47d2ad9e5291f317136be1
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-29 15:13:57 +0100

    k

---
+++ python-data_structures/1-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+   
+   result = ""
+   for i in matrix:
+        if i == str:
+            result = str
+   print ("{}""{}""{}""$".format(result))
