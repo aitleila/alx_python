820cfa4e4cec4ab3724b730d0616b9a2f342cfd4
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-29 15:23:49 +0100

    print matrix integer

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-   
-   result = ""
-   for i in matrix:
-        if i == str:
-            result = str
-   print ("{}""{}""{}""$".format(result))
+
+    for row in matrix:
+        for i, number in enumerate(row):
+            if i ==len(row) -1:
+                print("{:d}".format(number), end="")
+            else:
+                print("{:d}".format(number), end="")
+            print()
+    
