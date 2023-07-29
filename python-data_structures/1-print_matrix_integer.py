1cf94cbf1f6d1a397a787dea0ba6651a7f35c91e
aitleila <135441551+aitleila@users.noreply.github.com>
2023-07-29 16:33:07 +0100

    j

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
-def print_matrix_integer(matrix=[[]]):
+#!/usr/bin/pyton3
 
-    for row in matrix:
-        for i, number in enumerate(row):
-            if i ==len(row) - 1:
-                print ("{:d}".format(number))
+def print_matrix_integer(matrix=[[]]):
+       for row in matrix:
+        for i, num in enumerate(row):
+            if i == len(row) - 1:
+                print("{:d}".format(num), end="")
             else:
-                print ("{:d}".format(number))
-    print()
-    
+                print("{:d}".format(num), end=" ")
+        print()  
+        
