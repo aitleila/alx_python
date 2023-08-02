17a3176d0b2f77f499618652bf9110dfaef9460a
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-02 16:55:36 +0100

    n

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    for int in matrix:
-        new_matrix = (int*2, matrix)
+    for row in matrix:
+        for i, num in enumerate(row):
+            new_matrix = ({i**2}. format (num))
     return new_matrix
