d87d973eb6bcef0102f980fbe1dd8b167b1e2fe6
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-03 03:08:33 +0100

    m

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
 
-    for int in a_dictionary:
-        if int in a_dictionary:
-          best_key = max(a_dictionary, 'key', 'Value')
+    for key in a_dictionary:
+        if key in a_dictionary:
+          best_key = max(a_dictionary)
           return best_score
         
         else:
