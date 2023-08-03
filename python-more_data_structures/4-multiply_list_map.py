90531e06eb1643a07af40e6e25810121e81927d8
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-03 16:33:44 +0100

    k

--- python-more_data_structures/4-multiply_list_map.py
+++ python-more_data_structures/4-multiply_list_map.py
@@ -1,3 +1,3 @@
+#!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new_list=multiply_list_map(my_list*number)
-    return new_list
+    return multiply_list_map == (i**number for i in my_list)
