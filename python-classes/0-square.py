400b6832092c8ba3b56b03ee49a76f6c9338987c
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-04 20:32:41 +0100

    b

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python3
+
 class Square:
     pass
-    __password = 123
-    type = "Square"
-    size = 3
+    __size = int
 
-    def __init__(self, Square):
-        my_square = Square()
-        my_square.type = None
-        my_square.size = None
+    def __init__(self, size=0):
+        self.__size = size
+
+    def area (self):
+        self.__size**2
+
+    def perimeter (self):
+        4*self.__size
+
+    def set_size (self, new_size)
+        self._size = new_size
+        if new_size <=0 :
+            return ValueError ("The size is negative, please enter a new size.")
+        
+    def get_size (self):
+        self.__size
+
+
+    
+
+
 
 #Instantiation:
    
