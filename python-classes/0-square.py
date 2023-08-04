71b93e47b74c63042ca181070e1b7aca796bd792
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-04 19:14:21 +0100

    d

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 class Square:
+    pass
     __password = 123
-    name = "my_square"
-    size = "square"
-    value = 3
-    def __init__(self, square):
-        pass
+    type = "Square"
+    size = 3
+
+    def __init__(self, Square):
+        my_square = Square()
+        my_square.type = None
+        my_square.size = None
+
 #Instantiation:
    
 
