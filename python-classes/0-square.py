d6544cc98e53fa417b74c5416a66cd45ff021f88
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-04 21:13:08 +0100

    n

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -10,21 +10,21 @@ class Square:
     """
     
     def __init__(self, size=0):
-        self.__size = size
+        return self.__size == size
 
     def area (self):
-        self.__size**2
+        return self.__size**2
 
     def perimeter (self):
-        4*self.__size
+       return  4*self.__size
 
     def set_size (self, new_size):
         self._size = new_size
         if new_size <=0 :
-            return ValueError ("The size is negative, please enter a new size.")
+            raise ValueError ("The size is negative, please enter a new size.")
         
     def get_size (self):
-        self.__size
+        return self.__size
 
 
     
