02ce02e7475fa6886c71894b1f97345645f08793
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-04 21:31:39 +0100

    x

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -18,7 +18,7 @@ class Square:
         if not isinstance (self, int):
             TypeError("size must be an integer")
 
-        elif size < 0 :
+        if size < 0 :
             ValueError("size must be >= 0")
 
         self.__size = size
