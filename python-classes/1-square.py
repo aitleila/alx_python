fbf24a9e60369d6629fa2667cd4cb4af8e4b1fd4
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-04 21:01:27 +0100

    d

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -12,23 +12,23 @@ class Square:
     def __init__(self, size=0):
         self.__size = size
         if size < 0:
-            return ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
         if not isinstance (self, int):
-            return TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
 
     def area (self):
-        self.__size**2
+        return self.__size**2
 
     def perimeter (self):
-        4*self.__size
+        return 4*self.__size
 
     def set_size (self, new_size):
         self._size = new_size
         if new_size < 0 :
-            return ValueError ("The size is negative, please enter a new size.")
+            raise ValueError ("The size is negative, please enter a new size.")
         
     def get_size (self):
-        self.__size
+        return self.__size
 
 
     
