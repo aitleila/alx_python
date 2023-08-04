ce7c2c01557922d2085ed9d5c6825e54430d7d32
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-04 21:20:37 +0100

    d

---
+++ python-classes/1-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+        __size (int): The size of the square (private attribute)
+"""
+
+class Square:
+
+    """
+    This class represents a square with a given size.
+    """
+    
+    def __init__(self, size=0):
+        if size is isinstance :
+            TypeError("size must be an integer")
+        if size < 0 :
+            ValueError("size must be >= 0")
+        
+        self.__size = size
+
+    def area (self):
+        self.__size**2
+
+    def perimeter (self):
+       4*self.__size
+
+    def set_size (self, new_size):
+        self._size = new_size
+        if new_size < 0 :
+            ValueError ("The size is negative, please enter a new size.")
+        
+    def get_size (self):
+        self.__size
+
+
+    
+
+
+
+#Instantiation:
+   
+
+
