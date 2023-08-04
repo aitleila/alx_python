4165fac62819984e438b6d9f4be2391ef23561b3
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-04 21:03:49 +0100

    x

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -10,11 +10,14 @@ class Square:
     """
     
     def __init__(self, size=0):
-        self.__size = size
+
         if size < 0:
             raise ValueError("size must be >= 0")
         if not isinstance (self, int):
             raise TypeError("size must be an integer")
+        
+        self.__size = size
+
 
     def area (self):
         return self.__size**2
