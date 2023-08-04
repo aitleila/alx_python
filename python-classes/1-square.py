c9f0adb2d521fd8803e90e021bf48ba14f309de3
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-04 21:23:27 +0100

    x

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -10,10 +10,8 @@ class Square:
     """
     
     def __init__(self, size=0):
-
         if not isinstance (self, int):
             TypeError("size must be an integer")
-
         if size < 0 :
             ValueError("size must be >= 0")
         
