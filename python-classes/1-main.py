19b0bfc748ce0025f405cd1f90211ce58bb98003
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 01:17:00 +0100

    i

--- python-classes/1-main.py
+++ python-classes/1-main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 Square = __import__('1-square').Square
 
-my_square_1 = Square(3)
+my_square_1 = Square('3')
 print(type(my_square_1))
 print(my_square_1.__dict__)
 
@@ -20,7 +20,7 @@ except Exception as e:
     print(e)
 
 try:
-    my_square_3 = Square(3)
+    my_square_3 = Square("3")
     print(type(my_square_3))
     print(my_square_3.__dict__)
 except Exception as e:
