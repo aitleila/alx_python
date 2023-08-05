f048e58584dec18bff5f12c9b3fd700b7319fe78
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 03:06:09 +0100

    h

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -9,7 +9,7 @@ class Square:
     This class represents a square with a given size.
     """
     
-    def __init__(self, size=0):
+    def __init__(self, size):
         """
         Square object with a optional size
 
