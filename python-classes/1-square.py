335b90f17faf2f6a4ad46bc953a1894c5b5240be
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 04:03:14 +0100

    m

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -5,20 +5,15 @@
 
 class Square:
 
-    """
-    This class represents a square with a given size.
-    """
+    """ This class represent square of sizes """
     
     def __init__(self, size=0,):
 
-        """
-        size must be an integer, otherwise raise a TypeError
+        """size must be an integer, otherwise raise a TypeError"""
 
-        if size is less than 0, raise a ValueError
-        """
         if not isinstance(self, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if size < 0 :
             raise ValueError("size must be >= 0")
         
         self.__size = size
