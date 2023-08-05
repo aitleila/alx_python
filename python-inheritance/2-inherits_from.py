0dfb0cd3cff8e487c4ef66ba311c295e7ad92776
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 23:06:26 +0100

    m

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
+
 """
-A function that return True if the is an instance 
-of the class that inherit directly or indirectly
+A function that return True if the is an instance of the class that inherit directly or indirectly
 """
 
 def inherits_from(obj, a_class):
-
-    """This is subclass of a class 
-    that inherits directly or indirectly
+    """This is subclass of a class that inherits directly or indirectly
     """
-
     return (
         issubclass(type(obj), a_class) and 
         type(obj) is not a_class
         )
-
