8b443928460429c53a64f7d7ef67075551d7f85a
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 01:48:57 +0100

    c

---
+++ python-classes/3-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+        __size (int): The size of the square (private attribute)
+"""
+
+class Square:
+
+    """
+    This class represents a square with a given size.
+    """
+    
+    def __init__(self, size=0,):
+        """
+        size must be an integer, otherwise raise a TypeError
+
+        if size is less than 0, raise a ValueError
+        """
+        
+        if not isinstance (self, int):
+            TypeError("size must be an integer")
+        if size < 0:
+            ValueError("size must be >= 0")
+        self.__size = size
+
+    def area (self):
+        """
+            current square area
+        """
+        return self.__size**2
+        return area(self.__size**2)
+
+    def perimeter (self):
+       4*self.__size
+
+    def set_size (self, new_size):
+        self._size = new_size
+        if new_size < 0 :
+            ValueError ("The size is negative, please enter a new size.")
+        
+    def get_size (self):
+        self.__size
+        @property
+        def self_size(self, value):
+            if not isinstance (self, int):
+                TypeError("size must be an integer")
+            if size < 0:
+                ValueError("size must be >= 0")
+
+
+
+
+
+    
+
+
+
+#Instantiation:
+   
+
+
