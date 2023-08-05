f7087f7b5671854cf4ccecfae704456c30865394
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 23:06:51 +0100

    m

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -8,6 +8,6 @@ def inherits_from(obj, a_class):
     """This is subclass of a class that inherits directly or indirectly
     """
     return (
-        issubclass(type(obj), a_class) and 
-        type(obj) is not a_class
-        )
+      issubclass(type(obj), a_class) and 
+      type(obj) is not a_class
+    )
