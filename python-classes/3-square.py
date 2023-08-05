68af0c3ec7eb0295c02a08c9168e2ee399331f5b
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 15:29:55 +0100

    n

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -19,10 +19,7 @@ class Square:
             TypeError:If size is not an interger
             ValueError:If size is less than 0
         """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")      
+             
         self.__size = size
 
     @property
@@ -44,7 +41,7 @@ class Square:
             TypeError:If size is not an interger
             ValueError:If size is less than 0
         """
-        if not isinstance (self,value, int):
+        if not isinstance (value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
