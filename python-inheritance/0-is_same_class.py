0f8c0fd6f6531e39deffac39fc86ff2c67335425
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 16:26:52 +0100

    j

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""      """
-class is_same_class:
-    """  A function that returns true if the object is exactly an instance """
 
-    def is_same_class(obj, a_class):
-        """An instance of the class """
+"""  A function that returns true if the object is exactly an instance """
 
-        if is_same_class (obj, int):
-            return True
-        else:
-            return False
+def is_same_class(obj, a_class):
+    """An instance of the class """
+
+    if is_same_class (obj, int):
+        return True
+    else:
+        return False
