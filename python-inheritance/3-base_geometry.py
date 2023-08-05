5099a4a71cb84fb503bb7e7d6361c6b4d28e330f
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 22:50:42 +0100

    j

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,12 +6,18 @@ class BaseGeometry:
     """This is empty class"""
 
 class BaseGeometryMetaClass(type):
-    """__init_subclass"""
-    def __dir__(cls):
-        return [attribute for attribute in super.__dict__() if attribute !='__init_subclass__']
+    """Removing __init_subclass"""
+    def __dir__(cls) -> None:
+        attributes = super().__dict__()
+        n_attributes = []
+        for attr in attributes:
+            if attr !=' __init_subclass__':
+                n_attributes.append(attr)
+        return n_attributes
+
 
 class BaseGeometry(MetaClass=BaseGeometryMetaClass):
-    """Removing __init_subclass"""
+    """__init_subclass"""
     def __dir__(cls):
         return [attribute for attribute in super.__dict__() if attribute !='__init_subclass__']
     pass
