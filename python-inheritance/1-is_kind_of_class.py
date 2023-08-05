4284f50be17cf9b38de667fbc5f942040be32466
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 16:51:51 +0100

    j

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""A function that returns true if the object is or an instance of a class that inherited from, the specified class"""
+def is_kind_of_class(obj, a_class):
+    """An instance of the class """
+    return isinstance(obj, a_class)
