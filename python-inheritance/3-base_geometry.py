236fbba2ebb660a2614927f7caeea1e1560a6ee1
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 21:30:16 +0100

    l

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This is empty class"""
+"""  A base class representing geometry.
+    This class is intended to be used as a base for other geometry-related classes.
+    It currently does not have any attributes or methods defined."""
 class BaseGeometry:
     """This is empty class"""
 
@@ -12,3 +14,4 @@ class BaseGeometry(MetaClass=BaseGeometryMetaClass):
     """Removing __init_subclass"""
     def __dir__(cls):
         return [attribute for attribute in super.__dict__() if attribute !='__init_subclass__']
+    pass
