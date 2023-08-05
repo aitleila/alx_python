7db88594d1e7144a6e4b0efcd2a8db636264f926
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 02:53:54 +0100

    ,l

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -18,10 +18,9 @@ class Square:
         
         if type(size) is not int (self, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
 
     def area (self):
         """
--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -21,12 +21,12 @@ class Square:
             ValueError:If size is less than 0
         """
         
-        if type(size) is not int (self, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+    
+        self.__size = size
 
     @property
 
