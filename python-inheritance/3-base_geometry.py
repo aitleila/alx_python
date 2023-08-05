305bfb92ddab70ac7869ec2c63aafb81afe033f8
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 22:53:08 +0100

    n

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """  
     A base class representing geometry.
-    This class is intended to be used as a base for other geometry-related classes.
-    It currently does not have any attributes or methods defined.
+    
 """
 class BaseGeometry:
     """
