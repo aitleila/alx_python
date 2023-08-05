3da49d55bf63e07f6e293c334bea8f7d6697154d
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-06 00:30:38 +0100

    k

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -7,17 +7,14 @@ class BaseGeometry:
     """
     A base class representing geometry.
     """
-    def area(self):
-        if NotImplemented :
-            raise " area() is not implemented "
-        return super().__area__()
-
-
 
 class BaseGeometryMetaClass(type):
     def __dir__(cls):
         return[attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-    
+    def area(self):
+        if NotImplemented :
+            raise " area() is not implemented "
+        return super().__area__()
 class BaseGeometry(metaclass=BaseGeometryMetaClass):
     """
     A base class representing geometry
