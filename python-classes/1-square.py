af5e90ab1c43c7eb5eb679274760547fec4b5271
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 03:44:37 +0100

    h

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -18,7 +18,7 @@ class Square:
         """
         if not isinstance(self, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif self.__size < 0:
             raise ValueError("size must be >= 0")
         
         self.__size == size
@@ -33,7 +33,7 @@ class Square:
         self.__size = new_size
         if not isinstance(self, int):
             raise TypeError("size must be an integer")
-        elif new_size < 0:
+        elif self.__size < 0:
             raise ValueError("size must be >= 0")
         
     def get_size (self):
