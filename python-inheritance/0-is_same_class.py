c00f39cc4bd898bf5acb62ddac934bb8687fce73
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-05 16:21:06 +0100

    m

---
+++ python-inheritance/0-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""      """
+class is_same_class:
+    """   """
+
+    def is_same_class(obj, a_class):
+        """  """
+
+        if is_same_class (obj, int):
+            return True
+        else:
+            return False
