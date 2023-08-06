0dc6726841d5a941761f6ea72680023f7baa9c23
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-06 01:55:51 +0100

    n

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    A base class representing geometry.
+
+    """
+class BaseGeometry:
+    """
+    A base class representing geometry.
+    """
+        
+class BaseGeometryMetaClass(type):
+
+    def __dir__(cls):
+        return[attribute for attribute in super().__dir__() if attribute != '__init_subclass__'] 
+    
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
+    """
+    A base class representing geometry
+    """
+    def __dir__(self):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+    def area(self):
+        raise Exception('area() is not implemented')
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0 :
+            raise ValueError("{} must be greater than 0".format(name))
+        
+class Rectangle(BaseGeometry):
+    """ This is a class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        r= Rectangle(width, height)
+        if r is isinstance(r, int):
+            raise r
+        if r is not isinstance:
+            raise TypeError("{} must be an integer".format(r))
+        
+
