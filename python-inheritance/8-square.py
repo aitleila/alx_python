269172a575acbe6822b01efc4c5ed8b70cc92cc8
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-06 02:53:54 +0100

    n

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+    A base class representing geometry.
+
+    """
+class BaseGeometry:
+    """
+    A base class representing geometry.
+    """
+        
+class BaseGeometryMetaClass(type):
+
+    def __dir__(cls):
+        return[attribute for attribute in super().__dir__() if attribute != '__init_subclass__'] 
+    
+class BaseGeometry(metaclass=BaseGeometryMetaClass):
+    """
+    A base class representing geometry
+    """
+    def __dir__(self):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    
+    def area(self):
+        raise Exception('area() is not implemented')
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0 :
+            raise ValueError("{} must be greater than 0".format(name))
+        
+class Rectangle(BaseGeometry):
+    """ This is a class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """width and height must be positive integers, validated by integer_validator"""
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+    
+    def area (self):
+        area = self.__width * self.__height
+        return area
+    
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+    
+class Square(Rectangle):
+    """ This is a class Square that inherits from class Rectangle"""
+
+    def __init__(self, size):
+        self.__size = size
+        self.integer_validator("size", size)
+    
+    def area(self):
+        area = self.__size**2
+        return area
+    
