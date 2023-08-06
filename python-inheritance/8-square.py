af6ca396d670148561206b02ffe64ea02cc1fd5a
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-06 03:04:38 +0100

    b

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -30,32 +30,53 @@ class BaseGeometry(metaclass=BaseGeometryMetaClass):
             raise ValueError("{} must be greater than 0".format(name))
         
 class Rectangle(BaseGeometry):
-    """ This is a class that inherits from BaseGeometry"""
+    """ 
+    This is a class that inherits from BaseGeometry
+    """
 
     def __init__(self, width, height):
-        """width and height must be positive integers, validated by integer_validator"""
+        """
+        width and height must be positive integers, validated by integer_validator
+        """
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
     
     def area (self):
+        """ 
+        Calculate the area of the rectangle
+        """
         area = self.__width * self.__height
         return area
     
     def __str__(self):
+        """
+        Return a string representation of the rectangle
+        """
         return f"[Rectangle] {self.__width}/{self.__height}"
     
 class Square(Rectangle):
-    """ This is a class Square that inherits from class Rectangle"""
+    """ 
+    This is a class Square that inherits from class Rectangle
+    """
 
     def __init__(self, size):
+        """ 
+        Initialize a square with size
+        """
         self.__size = size
         self.integer_validator("size", size)
     
     def area(self):
+        """
+        Calculate the area of the square
+        """
         return self.__size**2
     
     def __str__(self):
+        """
+        Return a string representation of the square
+        """
         return f"[Square] {self.__size}"
     
