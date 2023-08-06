21c6875b438e950483153d99da45ac50db3c4f2b
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-06 04:00:02 +0100

    s

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+
+"""
+class Base:
+    """
+    
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        
+        """
+        if id is not None(value, int):
+            value = id
+            raise id
+        else:
+            new_value = id
+            return new_value
+        
+
