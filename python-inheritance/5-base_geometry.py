8e71c0b2f448084d026d0e8fcb8c9fa1df8e9dd4
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-06 01:18:36 +0100

    b

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -7,20 +7,19 @@ class BaseGeometry:
     """
     A base class representing geometry.
     """
-
-class BaseGeometryMetaClass(type):
-
-    def __dir__(cls):
-        return[attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-    
     def area(self):
         raise Exception('area() is not implemented')
     
     def integer_validator(self, name, value):
-        if value is isinstance(value, int):
+        if not isinstance(value, int):
             raise TypeError("<name> must be an integer")
         if value <= 0 :
-            raise ValueError("<name> must be greater than 0") 
+            raise ValueError("<name> must be greater than 0")
+        
+class BaseGeometryMetaClass(type):
+
+    def __dir__(cls):
+        return[attribute for attribute in super().__dir__() if attribute != '__init_subclass__'] 
     
 class BaseGeometry(metaclass=BaseGeometryMetaClass):
     """
