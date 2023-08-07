16769369d78b053cadecc265376f2e64f9262ddd
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-07 21:03:11 +0100

    r

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+This is a class rectangle that defines a square
+
+"""
+class Base:
+    """
+    This class represent the base class
+
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        The id here is a private instance attribute
+        """
+        if id is not None(value, int):
+            value = id
+            raise id
+        else:
+            new_value = id
+            return new_value
+        
+class Rectangle(Base):
+        """
+        This class represent rectangle of sizes
+        """
+
+    __width -> width
+    __height -> height
+    __x -> x
+    __y -> y
+
+    def get_width(self):
+        """
+        
+        """
+        return self._width
+    
+    def set_width(self, value):
+        """
+        
+        """
+        self._width = value
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        
+        """
+        def get_height (self):
+            return 
+
+
