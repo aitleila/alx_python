abd09ad36c735e112963779ec6c92797f2b60131
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-07 21:06:54 +0100

    n

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """
+This is a class rectangle that defines a square
 
 """
 class Base:
     """
-    
+    This class represent the base class
+
     """
     __nb_objects = 0
 
     def __init__(self, id=None):
         """
-        
+        The id here is a private instance attribute
+
         """
         if id is not None(value, int):
             value = id
