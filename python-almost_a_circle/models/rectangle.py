69e30c3d9490c689a20ab772158665f77207ab9f
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-08 01:53:30 +0100

    b

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,13 +28,13 @@ class Rectangle(Base):
     __width = Base.width
     def get_width(self):
         """
-                    
+        The getter of the attribute width
         """
         return self.width
         
     def set_width(self, value):
         """
-                
+        the setter of the attribute width
         """
         self.width = value
 
@@ -42,13 +42,13 @@ class Rectangle(Base):
 
     def get_height(self):
         """
-            
+        The getter of the attribute height
         """
         return self.height
     
     def set_height(self, value):
         """
-            
+        The setter of the attribute height
         """
         self.height = value
     
@@ -56,13 +56,13 @@ class Rectangle(Base):
     
     def get_x (self):
         """
-            
+        The getter of the attribute x
         """
         return self.x
     
     def set_x (self, value):
         """
-            
+        The setter of the attribute x   
         """
         self.x = value
 
@@ -70,21 +70,23 @@ class Rectangle(Base):
     
     def get_y (self):
         """
-            
+        The getter of the attribute y
         """
         return self.y
     
     def set_y (self, value):
         """
-            
+        The setter of the attribute y
         """
         self.y = value
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """
-        
+        Class constructor
+        Call the super class with with use the logic of the __init__ of the Base class
         """
         super(Base).__init__(width)
+        
         self.width = width
         super(Base).__init__(height)
         self.height = height
