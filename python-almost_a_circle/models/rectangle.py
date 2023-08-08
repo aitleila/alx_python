57220b1eafc37b11f221ab966e94b2b5df81c8c7
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-08 04:39:06 +0100

    b

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -85,8 +85,25 @@ class Rectangle(Base):
         The area of rectangle
         """
         return self.__width*self.__height
+    
+    def display(self):
+        """Display the character #"""
+        for _ in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            print( ' ' *  self.__x  +  '#'  *  self.__width)
+        for _ in range(self.__height):
+            print('#' * self.__width)
+    
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+    
+    def display(self):
+        """Display the character #"""
+        for _ in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            print( ' ' *  self.__x  +  '#'  *  self.__width)
 
     def update (self, *args):
         """
