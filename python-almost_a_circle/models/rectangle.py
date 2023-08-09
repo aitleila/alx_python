63a032ba81e92e33f0d560e5c663b7e83b8ea4a4
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-09 21:31:04 +0100

    n

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,26 +1,7 @@
 #!/usr/bin/python3
-"""
-This is a class rectangle that defines a square
+"""The file Rectangle inherit from Base"""
 
-"""
-class Base:
-    """
-    This class represent the base class
-
-    """
-    __nb_objects = 0
-
-    def __init__(self, id=None): 
-        """
-        The id here is a private instance attribute
-
-        """
-      
-        if id is not None:
-            self.id=id
-        else:
-            Base.__nb_objects +=1 
-            self.id = Base.__nb_objects
+from models.base import Base
         
 class Rectangle(Base):
     """
