4026f9f3957aab5caae891a6510655fdb6a96c22
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-10 02:01:56 +0100

    h

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -144,7 +144,11 @@ class Rectangle(Base):
             attributes = ["id", "width", "height", "x", "y"]
             for i, arg in enumerate(args):
                 setattr(self, attributes[i], arg)      
+        if len(kwargs)==2:
+            result = 1
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+            return result
         
