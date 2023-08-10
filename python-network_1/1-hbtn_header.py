3e0805ff4b8511fb276ab61f2a07c41c0b365ba9
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-10 04:18:41 +0100

    b

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -4,7 +4,7 @@ Make a HEAD request to a web page, and return the HTTP headers
 """
 import requests
 
-req = 'https://alu-intranet.hbtn.io/status'
+req = 'https://intranet.hbtn.io'
 response = requests.head(req)
 
 print(response)
