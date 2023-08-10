75705d7b84e70afcc20d13c5219867868a375e31
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-10 03:38:50 +0100

    n

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,17 +1,7 @@
-"""
-Python script that fetches https://alu-intranet.hbtn.io/status
-"""
 import requests
 
 """
 Python script that fetches https://alu-intranet.hbtn.io/status
 """
 req = requests.get('https://alu-intranet.hbtn.io/status')
-
-data = ()
-data ["body"]= 'Body response:'
-data['type'] = "- type: <class 'str'>"
-data ['content'] = '- content: OK'
-full_data = data['body']+data['type']+data['content']
-
-print (req.text, "{}" "{}" "{}".format(data["body"], data["type"], data["content"]))
+print (req.text)
