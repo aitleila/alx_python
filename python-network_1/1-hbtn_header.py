71eae100b677253087cc4916e3abd79d56aeec03
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-10 19:16:13 +0100

    n

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -5,7 +5,12 @@ Make a HEAD request to a web page, and return the HTTP headers
 import requests
 import sys
 
-url = 'https://alu-intranet.hbtn.io/status'
-response = requests.head(url)
 
-print(response)
+if len(sys.argv) != 2:
+    print("Usage: ./1-hbtn_header.py <https://alu-intranet.hbtn.io/status>")
+    sys.exit(1)
+
+req = sys.argv[1]
+response = requests.get(req)
+x_request_id = response.headers.get('X-Request-Id')
+print(x_request_id)
