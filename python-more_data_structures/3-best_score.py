4e1f6087724ca3231635096800cf635f041c02f4
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-13 19:50:45 +0100

    n

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-
-    for key in a_dictionary:
-
-        if key in a_dictionary:
-          best_score= max(a_dictionary, key=a_dictionary.get)
-          return best_score
-        else:
-           return None
+  if not a_dictionary:
+    return None
+  best_key= max(a_dictionary, key=a_dictionary.get)
+  return best_key
+      
                            
