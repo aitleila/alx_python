5057c1e2a36cf350341f22a6f3a79d779403aa42
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-13 17:49:41 +0100

    h

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Python script that takes your GitHub credentials (username and password) 
+and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+def main():
+    """The method used to get My GitHub username and password"""
+    if len(sys.argv) != 3:
+        print("Usage: python 6-my_github.py aitleila leila_dkrth0983t")
+        return
+        username = sys.argv[1]
+        token = sys.argv[2]
+
+        url = f"https://api.github.com/user"
+        response = requests.get(url,auth=(username,token))
+
+if __name__ == "__main__":
+    main()
