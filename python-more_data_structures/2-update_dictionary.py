ae35e97f3c6fe8c6630aafc815252c0ee7596cca
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-13 19:08:25 +0100

    l

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-
-   for key in a_dictionary:
-      if key in a_dictionary:
-         map(update_dictionary, [{"language": "Python"}])   
-      elif key not in a_dictionary:
-         map(update_dictionary, [{"City": "San Francisco"}])   
+    a_dictionary[key] = value
+    return a_dictionary   
          
-
-
