4805c7930293f05a8a18e9162788c4b3c8626433
aitleila <135441551+aitleila@users.noreply.github.com>
2023-08-13 16:48:47 +0100

    m

--- python-network_1/4-error_code.py
+++ python-network_1/4-error_code.py
@@ -4,10 +4,20 @@ Python script that takes in a URL, sends a request to the URL and displays the b
 """
 import requests
 import sys
-url='https://alu-intranet.hbtn.io/status'
-response = requests.get(url)
-r = requests.get(url)
 
-if r.status_code(sys) >= 400:
-    print("Error code:",response.status_code)
-    sys.exit
+def main():
+    """The method use to get the Error code"""
+    if len(sys.argv) != 2:
+        print("Usage: python 4-error_code.py https://intranet.hbtn.io")
+        return
+    
+    url = sys.argv[1]
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
+
+    else:
+        print(response.text)
+
+if __name__ == "__main__":
+    main()
